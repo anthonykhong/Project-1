14a2472ddfe9378e40f6bf57ed62f0f2019f3a9b
anthonykhong <anthonykhong95200@gmail.com>
2023-03-06 00:22:02 +0100

    Add pseudo code for app.js file

--- js/app.js
+++ js/app.js
@@ -1,9 +1,50 @@
 /*----- constants -----*/
 
+// Define constant which will hold an array of cards images
+
 /*----- state variables -----*/
+
+// Define timer variable
+// Define matchedCards variable
+// Define flippedCards variable
 
 /*----- cached elements  -----*/
 
+// Define a variable which will select card-container class from html
+// Define variable which will select timer class from html
+// Define variable which will select results
+// Define variable which will select .start button from html
+// Define variable which will select .restart button from html
+
 /*----- event listeners -----*/
 
+// Add event listener which listens to cards clicked
+// Add event listener which listens to event when start button is clicked
+// Add event listener which listens to event when restart button is clicked
+
 /*----- functions -----*/
+
+// Add initialize function
+// Holds timer variable
+// Holds matchedCards variable
+// Holds flippedCards variable
+// Holds render function
+// Holds start function
+// Holds shuffle function
+
+// Add render function
+// Holds renderTimer function
+// Holds renderResults function
+// Holds renderCards function
+
+// Add handleClick function
+
+// Add flipCards function
+
+// Add checkMatches function
+
+// Add start function
+
+// Add restart function
+
+// Add shuffle function
